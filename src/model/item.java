dfce48ebb192dd862c606bd7c0efea41e87eb803
setsochoto-dotcom <setsochoto@gmail.com>
2026-04-30 22:28:10 +0200

    Refactor item class and add validation methods
    Refactor item class to Item, add setters for price and quantity, and initialize quantity to 0.

--- src/model/item.java
+++ src/model/item.java
@@ -1,14 +1,24 @@
 package model;
 
-public class item {
+public class Item {
     private String name;
     private double price;
     private int quantity;
-
-    public item(String name, double price, int quantity) {
+public void setPrice(double price) {
+    if (price < 0) {
+        System.out.println("price cannot be negative");
+    } else { this.price = price;
+           }
+    public void setQuantity(int quantity){
+        if (quantity < 0){
+        System.out.println("Quantity cannot be negative");
+    } else {this.quantity = quantity;
+           }
+}
+    public Item(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
-        this.quantity = quantity;
+        this.quantity = 0;
     }
 
     public String getName() {
