bc95e7af6d7f186f0c81555e3ed0f232f603a233
setsochoto-dotcom <setsochoto@gmail.com>
2026-04-30 22:42:13 +0200

    Update item.java
    Fix bugs and add constructor overloading to item

--- src/model/item.java
+++ src/model/item.java
@@ -4,33 +4,36 @@ public class Item {
     private String name;
     private double price;
     private int quantity;
-public void setPrice(double price) {
-    if (price < 0) {
-        System.out.println("price cannot be negative");
-    } else { this.price = price;
-           }
-    public void setQuantity(int quantity){
-        if (quantity < 0){
-        System.out.println("Quantity cannot be negative");
-    } else {this.quantity = quantity;
-           }
-}
+    
     public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    } 
+    public Item(String name, double price) {
         this.name = name;
         this.price = price;
         this.quantity = 0;
     }
 
-    public String getName() {
-        return name;
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getQuantity() { return quantity; }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            System.out.println("Price cannot be negative.");
+        } else {
+            this.price = price;
+        }
     }
 
-    public double getPrice() {
-        return price;
-    }
-
-    public int getQuantity() {
-        return quantity;
+    public void setQuantity(int quantity) {
+        if (quantity < 0) {
+            System.out.println("Quantity cannot be negative.");
+        } else {
+            this.quantity = quantity;
+        }
     }
 
     public void addStock(int amount) {
@@ -39,5 +42,9 @@ public void setPrice(double price) {
 
     public void reduceStock(int amount) {
         quantity -= amount;
+    }
+
+    public void display() {
+        System.out.println(name + " | P" + price + " | Qty: " + quantity);
     }
 }
